6f0e2faed17ec18ec505a854ffc7e6c62813eaab
Jordy van Domselaar <jordy.m.van.domselaar@gmail.com>
2021-03-28 19:34:20 +0200

    First shot at adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,30 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-QZRBSN2E1F"
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
       },
     },
   ],
