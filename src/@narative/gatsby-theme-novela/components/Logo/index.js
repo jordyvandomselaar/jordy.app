933d2b2fa2661ffea2d5dd00704af7841655315f
Jordy van Domselaar <jordy.m.van.domselaar@gmail.com>
2021-03-28 18:33:14 +0200

    Add makeshift logo for now

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import image from "../../../../assets/logo.png";
+import styled from "@emotion/styled";
+
+const Image = styled.img`
+  height: 100px;
+`
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ * Logo comes with a property value called `fill`. `fill` is useful
+ * when you want to change your logo depending on the theme you are on.
+ */
+export default function Logo({ fill }) {
+    return (
+        <Image src={image} alt=""/>
+    );
+}
